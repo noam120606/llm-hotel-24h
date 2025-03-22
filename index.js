741d37a3ea6f7e8de2cc06c81b51ec0974bd4d33
Speeckby <lucienfrance02@gmail.com>
2025-03-22 21:52:46 +0100

    Version multiple non fonctionelle

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const fs = require('fs');
 const { ChatMistralAI } = require("@langchain/mistralai");
 const { HumanMessage } = require("@langchain/core/messages");
+const { ChatGoogleGenerativeAI } = require("@langchain/google-genai");
 
 require('dotenv').config();
 
-class MistralAI {
-    constructor() {
-        this.mistral = new ChatMistralAI({
-            model: "mistral-large-latest",
-            role: "user",
-            apiKey: process.env.MISTRALAI_API_KEY,
-            temperature:0
-          });
+class LLM {
+    constructor(type = "mistral") {
+        this.type = type;
+        switch(type) {
+            case "mistral":
+                this.llm = new ChatMistralAI({
+                    model: "mistral-large-latest",
+                    apiKey: process.env.MISTRALAI_API_KEY,
+                    temperature:0
+                  });
+                break;
+            case "gemini" :
+                this.llm = new ChatGoogleGenerativeAI({
+                    model: "gemini-1.5-pro",
+                    temperature: 0,
+                    maxRetries: 2,
+                    apiKey: process.env.GOOGLE_API_KEY
+                  });
+                break;
+        }
 
         this.loadTools();
     }
@@ -23,25 +36,40 @@ class MistralAI {
             this.toolsByName[file.split('.')[0]] = require(`./tools/${file}`);
             tools.push(require(`./tools/${file}`));
         })
-        this.mistral = this.mistral.bindTools(tools);
+        this.llm = this.llm.bindTools(tools);
     }
     async getResponse(userPrompt) {
-        let messages = [new HumanMessage(userPrompt)];
-        const aiMessage = await this.mistral.invoke(messages);
-        messages.push(aiMessage);
+        let messages;
+        switch (this.type) {
+            case "mistral":
+                messages = [new HumanMessage(userPrompt)];
+                const aiMessage = await this.llm.invoke(messages);
+                messages.push(aiMessage);
 
-        for (const toolCall of aiMessage.tool_calls) {
-            const selectedTool = this.toolsByName[toolCall.name];
-            const toolMessage = await selectedTool.invoke(toolCall);
-            messages.push(toolMessage);
-        }
+                for (const toolCall of aiMessage.tool_calls) {
+                    const selectedTool = this.toolsByName[toolCall.name];
+                    const toolMessage = await selectedTool.invoke(toolCall);
+                    messages.push(toolMessage);
+                }
 
-        return await this.mistral.invoke(messages)
+                return await this.llm.invoke(messages);
+                break;
+            case "gemini":
+                const toolRes = await this.llm.invoke([
+                    [
+                        "human",
+                        userPrompt,
+                    ],
+                ]);
+                console.log(toolRes)
+                break;
+        }   
+        
     }
 }
 
-const Mistral = new MistralAI();
+const ai = new LLM("gemini");
 
-Mistral.getResponse("Quel est le nom du client Sami").then((response) => {
-    console.log(response.content);
+ai.getResponse("Quel est le nom du client Sami").then((response) => {
+    console.log(response);
 });
